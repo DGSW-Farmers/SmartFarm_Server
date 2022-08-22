81b5c8a383e2d86f49a509260c71a56cd2071160
yongbin <rhdydqls1024@naver.com>
2022-08-22 15:57:56 +0900

    수경 센서 Repository 메서드 추가

--- src/main/java/com/project/smartFarm/global/repository/HydroponicsSensorDataRepository.java
+++ src/main/java/com/project/smartFarm/global/repository/HydroponicsSensorDataRepository.java
@@ -1,9 +1,18 @@
 package com.project.smartFarm.global.repository;
 
 import com.project.smartFarm.global.entity.HydroponicsSensorData;
+import com.project.smartFarm.global.type.HydroponicsSensorType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface HydroponicsSensorDataRepository extends JpaRepository<HydroponicsSensorData, Long> {
+
+    List<HydroponicsSensorData> findAllByType(HydroponicsSensorType type);
+
+    Optional<HydroponicsSensorData> findByTypeAndSensorId(HydroponicsSensorType type, int sensorId);
+
 }
