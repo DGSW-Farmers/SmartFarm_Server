061cbd2224fc29124d2c3e41457146067a929e24
yongbin <rhdydqls1024@naver.com>
2022-08-23 08:30:11 +0900

    토경 메서드 추가

--- src/main/java/com/project/smartFarm/global/repository/SoilSensorDataRepository.java
+++ src/main/java/com/project/smartFarm/global/repository/SoilSensorDataRepository.java
@@ -1,9 +1,18 @@
 package com.project.smartFarm.global.repository;
 
 import com.project.smartFarm.global.entity.SoilSensorData;
+import com.project.smartFarm.global.type.SoilSensorType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SoilSensorDataRepository extends JpaRepository<SoilSensorData, Long> {
+
+    List<SoilSensorData> findAllByType(SoilSensorType type);
+
+    Optional<SoilSensorData> findByTypeAndSensorId(SoilSensorType type, int sensorId);
+
 }
