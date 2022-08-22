7a58010f68f3104e9ebcfb469e6c832755b88533
yongbin <rhdydqls1024@naver.com>
2022-08-22 15:57:01 +0900

    수경 센서 값 가져오는 비즈니스 로직 작성

--- src/main/java/com/project/smartFarm/domain/hydroponics/service/HydroponicsService.java
+++ src/main/java/com/project/smartFarm/domain/hydroponics/service/HydroponicsService.java
@@ -1,9 +1,18 @@
 package com.project.smartFarm.domain.hydroponics.service;
 
+import com.project.smartFarm.domain.hydroponics.exception.SensorNotFoundException;
+import com.project.smartFarm.domain.hydroponics.presentation.dto.response.SensorDataDetailedResponse;
+import com.project.smartFarm.domain.hydroponics.presentation.dto.response.SensorDataListResponse;
+import com.project.smartFarm.domain.hydroponics.presentation.dto.response.SensorDataResponse;
+import com.project.smartFarm.global.entity.HydroponicsSensorData;
 import com.project.smartFarm.global.repository.HydroponicsRepository;
 import com.project.smartFarm.global.repository.HydroponicsSensorDataRepository;
+import com.project.smartFarm.global.type.HydroponicsSensorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +22,57 @@ public class HydroponicsService {
     private final HydroponicsSensorDataRepository hydroponicsSensorDataRepository;
 
     // 모든 센서 값
-    public void getSensor() {
+    public SensorDataListResponse getSensor() {
 
+        List<HydroponicsSensorData> list = hydroponicsSensorDataRepository.findAll();
+
+        List<SensorDataDetailedResponse> sensorList = list.stream().map(it ->
+            SensorDataDetailedResponse.builder()
+                    .sensorId(it.getSensorId())
+                    .type(it.getType())
+                    .value(it.getValue())
+                    .build()
+        ).collect(Collectors.toList());
+
+        return SensorDataListResponse.builder()
+                .sensorList(sensorList)
+                .build();
+    }
+    
+    // 센서 종류의 모든 센서 값 가져오기
+    public SensorDataListResponse getSensorByType(HydroponicsSensorType type) {
+
+        List<HydroponicsSensorData> list = hydroponicsSensorDataRepository.findAllByType(type);
+
+        List<SensorDataDetailedResponse> sensorList = list.stream().map(it ->
+            SensorDataDetailedResponse.builder()
+                    .sensorId(it.getSensorId())
+                    .type(it.getType())
+                    .value(it.getValue())
+                    .build()
+        ).collect(Collectors.toList());
+
+        return SensorDataListResponse.builder()
+                .sensorList(sensorList)
+                .build();
+    }
+
+    // 센서 종류와 센서 ID로 값 가져오기
+    public SensorDataResponse getSensorByTypeAndId(HydroponicsSensorType type, int sensorId) {
+        HydroponicsSensorData data = hydroponicsSensorDataRepository.findByTypeAndSensorId(type, sensorId)
+                .orElseThrow(() -> {
+                    throw SensorNotFoundException.EXCEPTION;
+                });
+
+        SensorDataDetailedResponse sensorData = SensorDataDetailedResponse.builder()
+                .sensorId(data.getSensorId())
+                .type(data.getType())
+                .value(data.getValue())
+                .build();
+
+        return SensorDataResponse.builder()
+                .sensorData(sensorData)
+                .build();
     }
 
 }
