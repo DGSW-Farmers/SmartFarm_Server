4de6147d033c9ad2d000485f26168e6a14665e2d
yongbin <rhdydqls1024@naver.com>
2022-08-23 08:29:05 +0900

    토경재배 비즈니스로직 작성

--- src/main/java/com/project/smartFarm/domain/soil/service/SoilService.java
+++ src/main/java/com/project/smartFarm/domain/soil/service/SoilService.java
@@ -1,9 +1,18 @@
 package com.project.smartFarm.domain.soil.service;
 
+import com.project.smartFarm.domain.soil.presentation.dto.response.SensorDataDetailedResponse;
+import com.project.smartFarm.domain.soil.presentation.dto.response.SensorDataListResponse;
+import com.project.smartFarm.domain.soil.presentation.dto.response.SensorDataResponse;
+import com.project.smartFarm.global.entity.SoilSensorData;
+import com.project.smartFarm.global.exception.SensorNotFoundException;
 import com.project.smartFarm.global.repository.SoilRepository;
 import com.project.smartFarm.global.repository.SoilSensorDataRepository;
+import com.project.smartFarm.global.type.SoilSensorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +22,56 @@ public class SoilService {
     private final SoilSensorDataRepository soilSensorDataRepository;
 
     // 모든 센서 값
-    public void getSensor() {
+    public SensorDataListResponse getSensor() {
+        List<SoilSensorData> list = soilSensorDataRepository.findAll();
 
+        List<SensorDataDetailedResponse> sensorList = list.stream().map(it ->
+            SensorDataDetailedResponse.builder()
+                    .sensorId(it.getSensorId())
+                    .type(it.getType())
+                    .value(it.getValue())
+                    .build()
+        ).collect(Collectors.toList());
+
+        return SensorDataListResponse.builder()
+                .sensorList(sensorList)
+                .build();
+    }
+
+    // 센서 종류의 모든 센서 값 가져오기
+    public SensorDataListResponse getSensorByType(SoilSensorType type) {
+
+        List<SoilSensorData> list = soilSensorDataRepository.findAllByType(type);
+
+        List<SensorDataDetailedResponse> sensorList = list.stream().map(it ->
+                SensorDataDetailedResponse.builder()
+                        .sensorId(it.getSensorId())
+                        .type(it.getType())
+                        .value(it.getValue())
+                        .build()
+        ).collect(Collectors.toList());
+
+        return SensorDataListResponse.builder()
+                .sensorList(sensorList)
+                .build();
+    }
+
+    // 센서 종류와 센서 ID로 값 가져오기
+    public SensorDataResponse getSensorByTypeAndId(SoilSensorType type, int sensorId) {
+        SoilSensorData data = soilSensorDataRepository.findByTypeAndSensorId(type, sensorId)
+                .orElseThrow(() -> {
+                    throw SensorNotFoundException.EXCEPTION;
+                });
+
+        SensorDataDetailedResponse sensorData = SensorDataDetailedResponse.builder()
+                .sensorId(data.getSensorId())
+                .type(data.getType())
+                .value(data.getValue())
+                .build();
+
+        return SensorDataResponse.builder()
+                .sensorData(sensorData)
+                .build();
     }
 
 }
