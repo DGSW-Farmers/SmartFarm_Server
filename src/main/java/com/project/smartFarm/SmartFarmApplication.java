922714aad99860f24ff42d3f59cf11096e5fe7af
yongbin <rhdydqls1024@naver.com>
2022-10-07 09:43:13 +0900

    :memo: :: apache tomcat 구성

--- src/main/java/com/project/smartFarm/SmartFarmApplication.java
+++ src/main/java/com/project/smartFarm/SmartFarmApplication.java
@@ -2,9 +2,10 @@ package com.project.smartFarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmartFarmApplication {
+public class SmartFarmApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartFarmApplication.class, args);
